bcaf79cd964ba8c2a08e5a9c86f6e4b2ddf739ba
Maguslibratis162 <51017800+Maguslibratis162@users.noreply.github.com>
2024-02-19 17:26:43 +0700

    Update

---
+++ Assets/My_Project/C#Script/IP_Manager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FishNet.Connection;
+using FishNet.Object;
+using FishNet.Transporting.Tugboat;
+using TMPro;
+
+public class IP_Manager : MonoBehaviour
+{
+    public Tugboat _tugboat;
+    public TMP_InputField _inputField;
+    // Start is called before the first frame update
+    void Start()
+    {
+       //_tugboat.SetClientAddress("");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void SetClientIP() 
+    {
+        _tugboat?.SetClientAddress(_inputField.text);
+    }
+
+}
